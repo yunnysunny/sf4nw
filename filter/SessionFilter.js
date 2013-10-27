f32e8e56e45cc6079c80faf27dd1440d6fcab567
白一梓 <yunnysunny@gmail.com>
2013-10-27 17:43:26 +0800

    修正了每次都会重新生成session的bug

--- filter/SessionFilter.js
+++ filter/SessionFilter.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var AbstractFilter = require('../lib/mvc/AbstractFilter');
 var Cookie = require('../lib/Cookie');
+var Session = require('../lib/store/Session');
 var config = require('../config');
 var sessionOption = config.SESSION_OPTION;
 var sessionManage = config.SESSION_MANAGE;
@@ -13,12 +14,21 @@ util.inherits(SessionFilter, AbstractFilter);
 
 function isValidValue(result, maxAge) {
 	var valid = false;
-	if (result) {
+	if (result instanceof Session) {
 		if (new Date().getTime() - result.getLastActive() < maxAge * 1000) {
 			valid = true;
 		}
 	}
 	return valid;
+}
+
+function addNewSession(response) {
+	var sessionid = sessionManage.create();
+	if (sessionid) {
+		var cookie = new Cookie(sessionOption.cookieName,sessionid,0,'/',null,true);
+		response.addCookie(cookie);
+	}
+	
 }
 
 SessionFilter.prototype.doFilter = function(request, response) {
@@ -28,11 +38,11 @@ SessionFilter.prototype.doFilter = function(request, response) {
 		var sessionResult = sessionManage.get(sessionid);
 		if (isValidValue(sessionResult, sessionOption.maxActiveTime)) {
 			request.session = sessionResult;
+		} else {
+			addNewSession(response);
 		}
 	} else {
-		var sessionid = sessionManage.create();
-		var cookie = new Cookie(sessionOption.cookieName,sessionid,0,'/',null,true);
-		response.addCookie(cookie);
+		addNewSession(response);
 	}
 	return true;
 }
